e96c8d8a73c5e361e77e64dbd73bab95073153e8
vmalabanan <vanessamalabanan@yahoo.com>
2022-07-15 19:41:33 -0700

    Initial Commit

---
+++ iOs-Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOs-Prework
+//
+//  Created by Vanessa Malabanan on 7/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOs-PreworkUITests/iOs_PreworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOs_PreworkUITestsLaunchTests.swift
+//  iOs-PreworkUITests
+//
+//  Created by Vanessa Malabanan on 7/15/22.
+//
+
+import XCTest
+
+class iOs_PreworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
